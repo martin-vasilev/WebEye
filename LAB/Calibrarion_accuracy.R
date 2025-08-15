181be73d708c4dfe2d33eb0b53fabab991d49b0b
Martin Vasilev <m.vasilev@psychlangsci.ucl.ac.uk>
2025-08-15 15:38:12 +0100

    add webcam calibration accuracy

--- LAB/Calibrarion_accuracy.R
+++ LAB/Calibrarion_accuracy.R
@@ -36,6 +36,8 @@ el_acc<- cal %>%
             Med= median(offset_deg, na.rm= T))
 
 mean(el_acc$M)
+sd(el_acc$M)
+range(el_acc$M)
 
 # generate a plot of 
 P_cal<- el_acc %>% 
@@ -57,5 +59,63 @@ P_cal<- el_acc %>%
   
 ggsave(filename = 'LAB/Plots/Eyelink_calibration_accuracy.png', plot = P_cal,
        width = 1600, height = 1600, units = 'px')
+
+
+####### Webcam reported calibration accuracy:
+
+get_num <- function(string) {
+  as.numeric(gsub("[^0-9]", "", string))
+}
+
+folders<- list.dirs('LAB/lab_raw_data')
+folders<- folders[2:length(folders)]
+
+web_acc<- NULL
+
+for(i in 1:length(folders)){
+  
+  # open files:
+  folder_dir<- folders[i]
+  
+  sub= unlist(strsplit(folder_dir, '/'))[3]
+  sub= get_num(sub)
+  
+  list<- substr(folder_dir, nchar(folder_dir), nchar(folder_dir))
+  
+  ts<- suppressMessages(read_csv(paste(folder_dir, '/timeseries.csv', sep='')))
+  ts$value<- as.numeric(ts$value)
+  ce<- subset(ts, is.element(variable_name, c('error_calibration_x', 'error_calibration_y')) & !is.na(value))
+  
+  ce<- ce%>% 
+    group_by(variable_name) %>% 
+    distinct(value)
+  
+  ce$sub<- sub
+  
+  web_acc<- rbind(web_acc, ce)
+  
+  unique(ce$value)
+  
+}
+
+web_acc$value[which(web_acc$variable_name== 'error_calibration_x')]<- web_acc$value[which(web_acc$variable_name== 'error_calibration_x')]*0.0187
+web_acc$value[which(web_acc$variable_name== 'error_calibration_y')]<- web_acc$value[which(web_acc$variable_name== 'error_calibration_y')]*0.0192
+
+web_acc<- web_acc %>%
+  group_by(sub, variable_name) %>%
+  summarise(mean_value = mean(value), .groups = "drop")%>%
+  pivot_wider(id_cols = sub,
+                  names_from = variable_name,
+                  values_from = mean_value)
+
+# x position:
+mean(web_acc$error_calibration_x)
+sd(web_acc$error_calibration_x)
+
+# y position:
+mean(web_acc$error_calibration_y)
+sd(web_acc$error_calibration_y)
+
+write.csv(x = web_acc, file = 'LAB/data/webcam_calibration.csv')
 
 
