2457b6ee94566c35fb6a3e7d02548b0636853ff2
Bernhard Angele <bernhard.angele@gmail.com>
2025-10-10 00:04:27 +0800

    First steps for corpus analysis

--- Data_analysis_Exp1_brms.R
+++ Data_analysis_Exp1_brms.R
@@ -214,9 +214,64 @@ qs_save(blmm_target_TVT_exp1_gaussian, "LAB/models/brms/blmm_target_TVT_exp1_gau
 
 # Corpus analysis
 
+# add column indicating which word (from column word_num) is the final word in the sentence (contains a ".")
+corpus_words_exp1 <- corpus_words_exp1 %>% mutate(position = case_when(grepl("\\.$", wordID) & word_num == max(word_num) ~ "last",
+                                                        word_num == 1 ~ "first",
+                                                        TRUE ~ "middle"),
+                                                  # make a word code uniquely identifying each word, combining item and word_num
+                                                  word_code = paste(item, word_num, sep = "_"))
+
 contrasts(corpus_words_exp1$Tracker) <- c(-.5,.5)
 ctr <- function(x) scale(x, scale = FALSE)
 
-lmm_corpus_words_FFD <- lmer(FFD ~ ctr(zipf) * Tracker + (Tracker+zipf  | sub) + (1 | item),
-                         data = corpus_words_exp1)
+lmm_corpus_words_FFD <- lmer(FFD ~ ctr(zipf) * Tracker + (Tracker+ctr(zipf)  | sub) + (1 | word_code),
+                         # exclude first and last word in a sentence and fixations < 80 ms and >800 ms
+                         data = corpus_words_exp1 %>% filter(position != "first" & position != "last" & FFD > 80 & FFD < 800))
 summary(lmm_corpus_words_FFD)
+
+# brms model for FFD
+
+blmm_corpus_FFD_exp1_exgaussian <-
+  brm(
+    # exclu
+    data = corpus_words_exp1,
+    formula = bf(
+      FFD ~ ctr(zipf) * Tracker  + (ctr(zipf) * Tracker | sub) + (1 | item),
+      beta ~ ctr(zipf) * Tracker  + (ctr(zipf) * Tracker | sub) + (1 | item)),
+    warmup = 1000,
+    iter = 5000,
+    chains = 4,
+    prior = priors_beta,
+    sample_prior = "yes",
+    family = exgaussian(),
+    init = "0",
+    control = list(adapt_delta = 0.8),#, max_treedepth = 15),
+    cores = 4,
+    backend = "cmdstanr",
+    threads = threading(2),
+    silent = 0
+  )
+
+qs_save(blmm_corpus_FFD_exp1_exgaussian, "LAB/models/brms/blmm_corpus_FFD_exp1_exgaussian.qs")
+
+# gaussian brms
+
+blmm_corpus_FFD_exp1_gaussian <- 
+  brm(
+    data = corpus_words_exp1,
+    formula = bf(
+      log(FFD) ~ ctr(zipf) * Tracker + (Tracker+ctr(zipf)  | sub) + (Tracker | word_code)),
+    warmup = 10,
+    iter = 50,
+    chains = 4,
+    prior = priors_gaussian,
+    sample_prior = "yes",
+    family = gaussian(),
+    #init = "0",
+    control = list(adapt_delta = 0.8),#, max_treedepth = 15),
+    cores = 4,
+    backend = "cmdstanr",
+    threads = threading(2),
+    silent = 0
+  )
+qs_save(blmm_corpus_FFD_exp1_gaussian, "LAB/models/brms/blmm_corpus_FFD_exp1_gaussian.qs")  
